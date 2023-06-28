9ad3c94cb092a3d0d2dc316d1b1386f2477f9c46
xmarchena50072-ufide <xmarchena50072@ufide.ac.cr>
2023-06-28 16:19:13 -0700

    Initial commit

---
+++ src/main/java/com/grupo_dos/proyecto_bd/Proyecto_bd.java
@@ -0,0 +1,34 @@
+package com.grupo_dos.proyecto_bd;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fidelitas
+ */
+public class Proyecto_bd {
+
+    public static void main(String[] args) {
+        try {
+            
+            Class.forName("oracle.jdbc.OracleDriver");
+            String url="jdbc:oracle:thin:@localhost:1521:orcl";
+            String uname="inventario";
+            String upass="DBFide1";
+            Connection conn= DriverManager.getConnection(url,uname,upass);
+            String sql="SELECT * FROM CATEGORIAS";
+            PreparedStatement pst= conn.prepareStatement(sql);
+            ResultSet rs=pst.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString(2));
+                
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        } finally {
+        }
+    }
+}
